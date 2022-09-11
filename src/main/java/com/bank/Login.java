dd370f8f3c064becbf5378e47ee6ba494252d748
Rishabh <rishabhgupta7199@gmail.com>
2022-09-11 06:10:00 -0700

    added BufferedReader in getUsernamePassword method

--- src/main/java/com/bank/Login.java
+++ src/main/java/com/bank/Login.java
@@ -2,6 +2,9 @@ package com.bank;
 
 import lombok.Data;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +14,21 @@ public class Login {
     private String password;
     private String role;
 
+    private final String FILE_PATH = "src/main/resources/userdata.csv";
+
     public boolean validateUser(String username, String password) {
         return username.equals(this.username) && password.equals(this.password);
     }
 
     public List<String> getUsernamePassword() {
         List<String> usernamePassword = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH));
+            reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        return usernamePassword;
     }
 }
