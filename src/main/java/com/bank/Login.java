e5697b0a087567cf76398ec18e6395203ec1d628
Rishabh <rishabhgupta7199@gmail.com>
2022-09-11 06:05:18 -0700

    added logic to validateUser

--- src/main/java/com/bank/Login.java
+++ src/main/java/com/bank/Login.java
@@ -6,4 +6,12 @@ import lombok.Data;
 public class Login {
     private String username;
     private String password;
+    private String role;
+
+    public boolean validateUser(String username, String password) {
+        if (username.equals(this.username) && password.equals(this.password)) {
+            return true;
+        }
+        return false;
+    }
 }
