90958e4df477ed2314f8b7b44d3c10ab37c381ab
Rishabh <rishabhgupta7199@gmail.com>
2022-09-12 03:22:01 -0700

    added encrypt function

--- src/main/java/com/bank/EncryptDycript.java
+++ src/main/java/com/bank/EncryptDycript.java
@@ -4,5 +4,18 @@ import lombok.Data;
 
 @Data
 public class EncryptDycript {
+    String usernameEncryptDycript(String username, char mode) {
+        return null;
+    }
+
+    String encrypt(String value) {
+        String enCoded = "";
+        char[] values = value.toCharArray();
+        int[] codedValues = new int[values.length];
+        for (int index = 0; index < values.length; index++) {
+            enCoded += values[index] + 1 + "%";
+        }
+        return enCoded;
+    }
 
 }
--- src/main/java/com/bank/Main.java
+++ src/main/java/com/bank/Main.java
@@ -3,6 +3,8 @@ package com.bank;
 public class Main {
     public static void main(String[] args) {
         Login login = new Login();
-        login.validateUser("rishabh", "rishabh@123");
+        EncryptDycript encryptDycript = new EncryptDycript();
+        System.out.println(encryptDycript.encrypt("rishabh"));
+
     }
 }
